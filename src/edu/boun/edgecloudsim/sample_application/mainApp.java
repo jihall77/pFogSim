2a57c252f73cb7e9b2be6dc32675ff5286f99562
cpj0009 <cpj0009@cse61.eng.auburn.edu>
2018-06-06 15:42:39 -0500

    Added test XML files (They are big)

--- src/edu/boun/edgecloudsim/sample_application/mainApp.java
+++ src/edu/boun/edgecloudsim/sample_application/mainApp.java
@@ -23,6 +23,9 @@ import edu.boun.edgecloudsim.core.SimSettings;
 import edu.boun.edgecloudsim.utils.SimLogger;
 import edu.boun.edgecloudsim.utils.SimUtils;
 
+import java.io.PrintWriter;
+import java.io.File;
+
 public class mainApp {
 	
 	/**
@@ -30,6 +33,370 @@ public class mainApp {
 	 */
 	public static void main(String[] args) {
 		//disable console output of cloudsim library
+		/*
+		 * @author CJ
+		 * trying to output the test files we want
+		 * 
+		 */
+		try{
+		    PrintWriter node = new PrintWriter("large_node_test.xml", "UTF-8");
+		    PrintWriter links = new PrintWriter("large_links_test.xml", "UTF-8");
+		    node.println("<?xml version=\"1.0\"?>");
+		    links.println("<?xml version=\"1.0\"?>");
+		    int counter = 1;
+		    
+		    node.println("<edge_devices>");
+		    links.println("<links>");
+		    
+		    //Make a the cloud
+		    node.println("<datacenter arch=\"x86\" os=\"Linux\" vmm=\"Xen\">\n");
+		    node.println("<costPerBw>0.1</costPerBw>\n" + 
+		    		"		<costPerSec>3.0</costPerSec>\n" + 
+		    		"		<costPerMem>0.05</costPerMem>\n" + 
+		    		"		<costPerStorage>0.1</costPerStorage>");
+		    node.println("<location>\n" + 
+		    		"			<x_pos>125</x_pos>\n" + 
+		    		"			<y_pos>125</y_pos>\n" + 
+		    		"			<level>0</level>" +
+		    		"			<wlan_id>0</wlan_id>\n" + 
+		    		"			<attractiveness>1</attractiveness>\n" + 
+		    		"		</location>");
+		    node.println("<hosts>\n" + 
+		    		"			<host>\n" + 
+		    		"				<core>8</core>\n" + 
+		    		"				<mips>4000</mips>\n" + 
+		    		"				<ram>8000</ram>\n" + 
+		    		"				<storage>200000</storage>\n" + 
+		    		"				<VMs>\n" + 
+		    		"					<VM vmm=\"Xen\">\n" + 
+		    		"						<core>2</core>\n" + 
+		    		"						<mips>1000</mips>\n" + 
+		    		"						<ram>2000</ram>\n" + 
+		    		"						<storage>50000</storage>\n" + 
+		    		"					</VM>\n" + 
+		    		"					<VM vmm=\"Xen\">\n" + 
+		    		"						<core>2</core>\n" + 
+		    		"						<mips>1000</mips>\n" + 
+		    		"						<ram>2000</ram>\n" + 
+		    		"						<storage>50000</storage>\n" + 
+		    		"					</VM>\n" + 
+		    		"				</VMs>\n" + 
+		    		"			</host>\n" + 
+		    		"		</hosts>\n" + 
+		    		"	</datacenter>");
+		    
+		    int level;
+		    for(int i = 0; i < 5; i++)
+		    {
+		    	level = 1;
+		    	int x = (int) (Math.random()* 250);
+		    	int y = (int) (Math.random()* 250);
+
+		    	//Generate random x and y
+		    	
+		    	//Add node
+		    	node.println("<datacenter arch=\"x86\" os=\"Linux\" vmm=\"Xen\">\n");
+			    node.println("<costPerBw>0.1</costPerBw>\n" + 
+			    		"		<costPerSec>3.0</costPerSec>\n" + 
+			    		"		<costPerMem>0.05</costPerMem>\n" + 
+			    		"		<costPerStorage>0.1</costPerStorage>");
+			    node.println("<location>\n" + 
+			    		"			<x_pos>" + x + "</x_pos>\n" + 
+			    		"			<y_pos>" + y + "</y_pos>\n" + 
+			    		"			<wlan_id>" + counter + "</wlan_id>\n" + 
+			    		"			<level>" + level + "</level>" +
+			    		"			<attractiveness>1</attractiveness>\n" + 
+			    		"		</location>");
+			    node.println("<hosts>\n" + 
+			    		"			<host>\n" + 
+			    		"				<core>8</core>\n" + 
+			    		"				<mips>4000</mips>\n" + 
+			    		"				<ram>8000</ram>\n" + 
+			    		"				<storage>200000</storage>\n" + 
+			    		"				<VMs>\n" + 
+			    		"					<VM vmm=\"Xen\">\n" + 
+			    		"						<core>2</core>\n" + 
+			    		"						<mips>1000</mips>\n" + 
+			    		"						<ram>2000</ram>\n" + 
+			    		"						<storage>50000</storage>\n" + 
+			    		"					</VM>\n" + 
+			    		"					<VM vmm=\"Xen\">\n" + 
+			    		"						<core>2</core>\n" + 
+			    		"						<mips>1000</mips>\n" + 
+			    		"						<ram>2000</ram>\n" + 
+			    		"						<storage>50000</storage>\n" + 
+			    		"					</VM>\n" + 
+			    		"				</VMs>\n" + 
+			    		"			</host>\n" + 
+			    		"		</hosts>\n" + 
+			    		"	</datacenter>");
+			    //Add links from this to the cloud
+			    links.println("<link>\n" + 
+			    		"		<name>L" + i + "_" + 0 + "</name>\n" + 
+			    		"		<left>\n" + 
+			    		"			<x_pos>" + x + "</x_pos>\n" + 
+			    		"			<y_pos>" + y + "</y_pos>\n" + 
+			    		"		</left>\n" + 
+			    		"		<right>\n" + 
+			    		"			<x_pos>0</x_pos>\n" + 
+			    		"			<y_pos>0</y_pos>\n" + 
+			    		"		</right>\n" + 
+			    		"		<left_latency>0.5</left_latency>\n" + 
+			    		"		<right_latency>0.5</right_latency>\n" + 
+			    		"	</link>");
+			    //Add the sub nodes for this
+			    counter++;
+			    for(int j = 0; j < 5; j++)
+			    {
+			    	level = 2;
+			    	int x2 = (int) (Math.random()* 250);
+			    	int y2 = (int) (Math.random()* 250);
+
+			    	//Add node
+			    	node.println("<datacenter arch=\"x86\" os=\"Linux\" vmm=\"Xen\">\n");
+				    node.println("<costPerBw>0.1</costPerBw>\n" + 
+				    		"		<costPerSec>3.0</costPerSec>\n" + 
+				    		"		<costPerMem>0.05</costPerMem>\n" + 
+				    		"		<costPerStorage>0.1</costPerStorage>");
+				    node.println("<location>\n" + 
+				    		"			<x_pos>" + x2 + "</x_pos>\n" + 
+				    		"			<y_pos>" + y2 + "</y_pos>\n" + 
+				    		"			<wlan_id>" + counter + "</wlan_id>\n" + 
+				    		"			<level>" + level + "</level>" +
+				    		"			<attractiveness>1</attractiveness>\n" + 
+				    		"		</location>");
+				    node.println("<hosts>\n" + 
+				    		"			<host>\n" + 
+				    		"				<core>8</core>\n" + 
+				    		"				<mips>4000</mips>\n" + 
+				    		"				<ram>8000</ram>\n" + 
+				    		"				<storage>200000</storage>\n" + 
+				    		"				<VMs>\n" + 
+				    		"					<VM vmm=\"Xen\">\n" + 
+				    		"						<core>2</core>\n" + 
+				    		"						<mips>1000</mips>\n" + 
+				    		"						<ram>2000</ram>\n" + 
+				    		"						<storage>50000</storage>\n" + 
+				    		"					</VM>\n" + 
+				    		"					<VM vmm=\"Xen\">\n" + 
+				    		"						<core>2</core>\n" + 
+				    		"						<mips>1000</mips>\n" + 
+				    		"						<ram>2000</ram>\n" + 
+				    		"						<storage>50000</storage>\n" + 
+				    		"					</VM>\n" + 
+				    		"				</VMs>\n" + 
+				    		"			</host>\n" + 
+				    		"		</hosts>\n" + 
+				    		"	</datacenter>");
+			    	//Add link to parent
+				    links.println("<link>\n" + 
+				    		"		<name>L" + j + "_" + i + "</name>\n" + 
+				    		"		<left>\n" + 
+				    		"			<x_pos>" + x2 + "</x_pos>\n" + 
+				    		"			<y_pos>" + y2 + "</y_pos>\n" + 
+				    		"		</left>\n" + 
+				    		"		<right>\n" + 
+				    		"			<x_pos>" + x + "</x_pos>\n" + 
+				    		"			<y_pos>" + y + "</y_pos>\n" + 
+				    		"		</right>\n" + 
+				    		"		<left_latency>0.5</left_latency>\n" + 
+				    		"		<right_latency>0.5</right_latency>\n" + 
+				    		"	</link>");
+			    	//Add the sub nodes
+			    	counter++;
+			    	for(int k = 0; k < 2; k++)
+			    	{
+			    		level = 3;
+				    	int x3 = (int) (Math.random()* 250);
+				    	int y3 = (int) (Math.random()* 250);
+
+			    		//Add node
+				    	node.println("<datacenter arch=\"x86\" os=\"Linux\" vmm=\"Xen\">\n");
+					    node.println("<costPerBw>0.1</costPerBw>\n" + 
+					    		"		<costPerSec>3.0</costPerSec>\n" + 
+					    		"		<costPerMem>0.05</costPerMem>\n" + 
+					    		"		<costPerStorage>0.1</costPerStorage>");
+					    node.println("<location>\n" + 
+					    		"			<x_pos>" + x3 + "</x_pos>\n" + 
+					    		"			<y_pos>" + y3 + "</y_pos>\n" + 
+					    		"			<wlan_id>" + counter + "</wlan_id>\n" + 
+					    		"			<level>" + level + "</level>" +
+					    		"			<attractiveness>1</attractiveness>\n" + 
+					    		"		</location>");
+					    node.println("<hosts>\n" + 
+					    		"			<host>\n" + 
+					    		"				<core>8</core>\n" + 
+					    		"				<mips>4000</mips>\n" + 
+					    		"				<ram>8000</ram>\n" + 
+					    		"				<storage>200000</storage>\n" + 
+					    		"				<VMs>\n" + 
+					    		"					<VM vmm=\"Xen\">\n" + 
+					    		"						<core>2</core>\n" + 
+					    		"						<mips>1000</mips>\n" + 
+					    		"						<ram>2000</ram>\n" + 
+					    		"						<storage>50000</storage>\n" + 
+					    		"					</VM>\n" + 
+					    		"					<VM vmm=\"Xen\">\n" + 
+					    		"						<core>2</core>\n" + 
+					    		"						<mips>1000</mips>\n" + 
+					    		"						<ram>2000</ram>\n" + 
+					    		"						<storage>50000</storage>\n" + 
+					    		"					</VM>\n" + 
+					    		"				</VMs>\n" + 
+					    		"			</host>\n" + 
+					    		"		</hosts>\n" + 
+					    		"	</datacenter>");
+			    		//Add link to parent
+					    links.println("<link>\n" + 
+					    		"		<name>L" + k + "_" + j + "</name>\n" + 
+					    		"		<left>\n" + 
+					    		"			<x_pos>" + x3 + "</x_pos>\n" + 
+					    		"			<y_pos>" + y3 + "</y_pos>\n" + 
+					    		"		</left>\n" + 
+					    		"		<right>\n" + 
+					    		"			<x_pos>" + x2 + "</x_pos>\n" + 
+					    		"			<y_pos>" + y2 + "</y_pos>\n" + 
+					    		"		</right>\n" + 
+					    		"		<left_latency>0.5</left_latency>\n" + 
+					    		"		<right_latency>0.5</right_latency>\n" + 
+					    		"	</link>");
+			    		//Add the sub nodes
+			    		counter++;
+			    		for(int z = 0; z < 4; z++)
+			    		{
+			    			level = 4;
+					    	int x4 = (int) (Math.random()* 250);
+					    	int y4 = (int) (Math.random()* 250);
+
+			    			//Add node 
+					    	node.println("<datacenter arch=\"x86\" os=\"Linux\" vmm=\"Xen\">\n");
+						    node.println("<costPerBw>0.1</costPerBw>\n" + 
+						    		"		<costPerSec>3.0</costPerSec>\n" + 
+						    		"		<costPerMem>0.05</costPerMem>\n" + 
+						    		"		<costPerStorage>0.1</costPerStorage>");
+						    node.println("<location>\n" + 
+						    		"			<x_pos>" + x4 + "</x_pos>\n" + 
+						    		"			<y_pos>" + y4 + "</y_pos>\n" + 
+						    		"			<wlan_id>" + counter + "</wlan_id>\n" + 
+						    		"			<level>" + level + "</level>" +
+						    		"			<attractiveness>1</attractiveness>\n" + 
+						    		"		</location>");
+						    node.println("<hosts>\n" + 
+						    		"			<host>\n" + 
+						    		"				<core>8</core>\n" + 
+						    		"				<mips>4000</mips>\n" + 
+						    		"				<ram>8000</ram>\n" + 
+						    		"				<storage>200000</storage>\n" + 
+						    		"				<VMs>\n" + 
+						    		"					<VM vmm=\"Xen\">\n" + 
+						    		"						<core>2</core>\n" + 
+						    		"						<mips>1000</mips>\n" + 
+						    		"						<ram>2000</ram>\n" + 
+						    		"						<storage>50000</storage>\n" + 
+						    		"					</VM>\n" + 
+						    		"					<VM vmm=\"Xen\">\n" + 
+						    		"						<core>2</core>\n" + 
+						    		"						<mips>1000</mips>\n" + 
+						    		"						<ram>2000</ram>\n" + 
+						    		"						<storage>50000</storage>\n" + 
+						    		"					</VM>\n" + 
+						    		"				</VMs>\n" + 
+						    		"			</host>\n" + 
+						    		"		</hosts>\n" + 
+						    		"	</datacenter>");
+			    			//Add link to parent
+						    links.println("<link>\n" + 
+						    		"		<name>L" + z + "_" + k + "</name>\n" + 
+						    		"		<left>\n" + 
+						    		"			<x_pos>" + x4 + "</x_pos>\n" + 
+						    		"			<y_pos>" + y4 + "</y_pos>\n" + 
+						    		"		</left>\n" + 
+						    		"		<right>\n" + 
+						    		"			<x_pos>" + x3 + "</x_pos>\n" + 
+						    		"			<y_pos>" + y3 + "</y_pos>\n" + 
+						    		"		</right>\n" + 
+						    		"		<left_latency>0.5</left_latency>\n" + 
+						    		"		<right_latency>0.5</right_latency>\n" + 
+						    		"	</link>");
+						    //Add subnodes
+					    	counter++;
+					    	for(int v = 0; v < 4; v++)
+				    		{
+				    			level = 5;
+						    	int x5 = (int) (Math.random()* 250);
+						    	int y5 = (int) (Math.random()* 250);
+
+				    			//Add node 
+						    	node.println("<datacenter arch=\"x86\" os=\"Linux\" vmm=\"Xen\">\n");
+							    node.println("<costPerBw>0.1</costPerBw>\n" + 
+							    		"		<costPerSec>3.0</costPerSec>\n" + 
+							    		"		<costPerMem>0.05</costPerMem>\n" + 
+							    		"		<costPerStorage>0.1</costPerStorage>");
+							    node.println("<location>\n" + 
+							    		"			<x_pos>" + x5 + "</x_pos>\n" + 
+							    		"			<y_pos>" + y5 + "</y_pos>\n" + 
+							    		"			<wlan_id>" + counter + "</wlan_id>\n" + 
+							    		"			<level>" + level + "</level>" +
+							    		"			<attractiveness>3</attractiveness>\n" + 
+							    		"		</location>");
+							    node.println("<hosts>\n" + 
+							    		"			<host>\n" + 
+							    		"				<core>8</core>\n" + 
+							    		"				<mips>4000</mips>\n" + 
+							    		"				<ram>8000</ram>\n" + 
+							    		"				<storage>200000</storage>\n" + 
+							    		"				<VMs>\n" + 
+							    		"					<VM vmm=\"Xen\">\n" + 
+							    		"						<core>2</core>\n" + 
+							    		"						<mips>1000</mips>\n" + 
+							    		"						<ram>2000</ram>\n" + 
+							    		"						<storage>50000</storage>\n" + 
+							    		"					</VM>\n" + 
+							    		"					<VM vmm=\"Xen\">\n" + 
+							    		"						<core>2</core>\n" + 
+							    		"						<mips>1000</mips>\n" + 
+							    		"						<ram>2000</ram>\n" + 
+							    		"						<storage>50000</storage>\n" + 
+							    		"					</VM>\n" + 
+							    		"				</VMs>\n" + 
+							    		"			</host>\n" + 
+							    		"		</hosts>\n" + 
+							    		"	</datacenter>");
+				    			//Add link to parent
+							    links.println("<link>\n" + 
+							    		"		<name>L" + v + "_" + z + "</name>\n" + 
+							    		"		<left>\n" + 
+							    		"			<x_pos>" + x5 + "</x_pos>\n" + 
+							    		"			<y_pos>" + y5 + "</y_pos>\n" + 
+							    		"		</left>\n" + 
+							    		"		<right>\n" + 
+							    		"			<x_pos>" + x4 + "</x_pos>\n" + 
+							    		"			<y_pos>" + y4 + "</y_pos>\n" + 
+							    		"		</right>\n" + 
+							    		"		<left_latency>0.5</left_latency>\n" + 
+							    		"		<right_latency>0.5</right_latency>\n" + 
+							    		"	</link>");
+						    	counter++;
+				    		}
+					    	
+			    		}
+			    	}
+			    }
+		    }
+		    
+		    
+		    
+		    
+		    links.println("</links>");
+		    node.println("</edge_devices>");
+		    links.close();
+		    node.close();
+		} catch (Exception e) {
+		   System.out.println("Failure has occurred");
+		}
+		
+		
+		
 		Log.disable();
 		//enable console output and file output of this application
 		SimLogger.enablePrintLog();
@@ -42,7 +409,8 @@ public class mainApp {
 		String outputFolder = "";
 		String edgeDevicesFile = "";
 		String applicationsFile = "";
-		String linksFile = "scripts/sample_application/config/links_test.xml";
+		//String linksFile = "scripts/sample_application/config/links_test.xml";
+		String linksFile = "large_links_test.xml";
 		if (args.length == 5){
 			configFile = args[0];
 			edgeDevicesFile = args[1];
@@ -54,7 +422,8 @@ public class mainApp {
 			SimLogger.printLine("Simulation setting file, output folder and iteration number are not provided! Using default ones...");
 			configFile = "scripts/sample_application/config/default_config.properties";
 			applicationsFile = "scripts/sample_application/config/applications.xml";
-			edgeDevicesFile = "scripts/sample_application/config/edge_devices_test.xml";
+			//edgeDevicesFile = "scripts/sample_application/config/edge_devices_test.xml";
+			edgeDevicesFile = "large_node_test.xml";
 			outputFolder = "sim_results/ite" + iterationNumber;
 		}
 
