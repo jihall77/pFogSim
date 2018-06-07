621bbdca44bb9d94ab8617ab4aefa8dbc160ea33
cpj0009 <cpj0009@cse61.eng.auburn.edu>
2018-06-07 10:20:51 -0500

    Started puddle

---
+++ src/edu/auburn/pFogSim/puddle/Puddle.java
@@ -0,0 +1,21 @@
+package edu.auburn.pFogSim.netsim;
+
+import edu.auburn.pFogSim.netsim.*;
+
+public class Puddle {
+	
+	// head = some head
+	//list = list of nodes on level
+	//node up = next puddle head location
+	//list of nodes downward puddle heads
+	
+	private Node head;
+	private List<Node> members;
+	private Node up;
+	private List<Node> down;
+	
+	public Puddle() {
+		
+	}
+	
+}
