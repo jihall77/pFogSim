fbc41a7fbc5dff7a54527efd1cf71932901011d3
jaybum <39962554+jaybum@users.noreply.github.com>
2018-06-15 11:39:08 -0500

    Update plotAvgProcessingTime.m

--- scripts/sample_application/matlab/plotAvgProcessingTime.m
+++ scripts/sample_application/matlab/plotAvgProcessingTime.m
@@ -5,7 +5,7 @@ function [] = plotAvgProcessingTime()
     plotGenericResult(1, 6, 'Processing Time for Health App (sec)', 'HEALTH_APP', 0);
     plotGenericResult(1, 6, 'Processing Time for Infotainment App (sec)', 'INFOTAINMENT_APP', 0);
     plotGenericResult(1, 6, 'Processing Time for Heavy Comp. App (sec)', 'HEAVY_COMP_APP', 0);
-
+    %{
     plotGenericResult(2, 6, 'Processing Time on Cloudlet (sec)', 'ALL_APPS', 0);
     plotGenericResult(2, 6, {'Processing Time on Cloudlet';'for Augmented Reality App (sec)'}, 'AUGMENTED_REALITY', 0);
     plotGenericResult(2, 6, {'Processing Time on Cloudlet';'for Health App (sec)'}, 'HEALTH_APP', 0);
@@ -17,4 +17,5 @@ function [] = plotAvgProcessingTime()
     plotGenericResult(3, 6, {'Processing Time on Cloud';'for Health App (sec)'}, 'HEALTH_APP', 0);
     plotGenericResult(3, 6, {'Processing Time on Cloud';'for Infotainment App (sec)'}, 'INFOTAINMENT_APP', 0);
     plotGenericResult(3, 6, {'Processing Time on Cloud';'for Heavy Computation App (sec)'}, 'HEAVY_COMP_APP', 0);
+    %}
 end
