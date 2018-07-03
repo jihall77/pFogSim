35c96d57a71a43518f11574b49e5495b4f554c4c
jih0007 <jih0007@cse25.eng.auburn.edu>
2018-07-03 16:36:55 -0500

    more fixing stuff

--- src/edu/auburn/pFogSim/util/DataInterpreter.java
+++ src/edu/auburn/pFogSim/util/DataInterpreter.java
@@ -15,16 +15,17 @@ import edu.boun.edgecloudsim.utils.SimLogger;
 
 public class DataInterpreter {
 	private static int MAX_LEVELS = 7;
-	private static String[][] nodeSpecs = new String[MAX_LEVELS][14];// the specs for all layers of the fog devices
+	private static String[][] nodeSpecs = new String[MAX_LEVELS][13];// the specs for all layers of the fog devices
+	private File xmlFile = null;
+	private FileWriter xmlFW = null;
+	private BufferedWriter xmlBR = null;
 	
-	public static void readFile(String inputfile, String outputfile) throws IOException {
+	public static void readFile(String inputfile, String outputfile, int level) throws IOException {
 		File dataFile = new File(inputfile);
 		FileReader dataFR = null;
 		BufferedReader dataBR = null;
 		
-		File xmlFile = new File(outputfile);
-		FileWriter xmlFW = new FileWriter(xmlFile);
-		BufferedWriter xmlBR = new BufferedWriter(xmlFW);
+		
 		
 		String rawNode = null;
 		String[] nodeLoc = new String[3];
@@ -46,9 +47,30 @@ public class DataInterpreter {
 	}
 	
 	public static void main(String[] args) throws IOException {
-		readFile("scripts/sample_application/config/Chicago_Schools.csv", "schoolNode.xml");
+		readFile("scripts/sample_application/config/Chicago_Schools.csv", "schoolNode.xml", 1);
 	}
-	
+	/**
+	 * the great beast...<br><br>
+	 * 
+	 * this is where we define the specs for the machines on the network.<br>
+	 * if you have a file to read these from go ahead, we, however, are defining them by hand.<br>
+	 * the first dimension of the array represents the fog layer<br>
+	 * the second dimension represents the particular attribute<br><br>
+	 * 
+	 *   0 - architecture<br>
+	 *   1 - operating systems<br>
+	 *   2 - virtual machine manager<br>
+	 *   3 - cost per Bandwidth<br>
+	 *   4 - cost per second<br>
+	 *   5 - cost per memory<br>
+	 *   6 - cost per storage<br>
+	 *   7 - is node a wifi access point<br>
+	 *   8 - is fog node moving<br>
+	 *   9 - number of cores for the machine<br>
+	 *  10 - million instructions per second (mips)<br>
+	 *  11 - ram<br>
+	 *  12 - storage 
+	 */
 	public static void initialize() {
 		nodeSpecs[MAX_LEVELS - 1][0] = "Cloud";
 		nodeSpecs[MAX_LEVELS - 1][1] = "Linux";
@@ -57,12 +79,89 @@ public class DataInterpreter {
 		nodeSpecs[MAX_LEVELS - 1][4] = "3.0";
 		nodeSpecs[MAX_LEVELS - 1][5] = "0.05";
 		nodeSpecs[MAX_LEVELS - 1][6] = "0.1";
-		nodeSpecs[MAX_LEVELS - 1][7] = "0";
+		nodeSpecs[MAX_LEVELS - 1][7] = "false";
 		nodeSpecs[MAX_LEVELS - 1][8] = "false";
-		nodeSpecs[MAX_LEVELS - 1][9] = "false";
-		nodeSpecs[MAX_LEVELS - 1][10] = "8000";
-		nodeSpecs[MAX_LEVELS - 1][11] = "500000000";
-		nodeSpecs[MAX_LEVELS - 1][12] = "40000000";
-		nodeSpecs[MAX_LEVELS - 1][13] = "100000000000";
+		nodeSpecs[MAX_LEVELS - 1][9] = "8000";
+		nodeSpecs[MAX_LEVELS - 1][10] = "500000000";
+		nodeSpecs[MAX_LEVELS - 1][11] = "40000000";
+		nodeSpecs[MAX_LEVELS - 1][12] = "100000000000";
+		nodeSpecs[MAX_LEVELS - 2][0] = "x86";
+		nodeSpecs[MAX_LEVELS - 2][1] = "Linux";
+		nodeSpecs[MAX_LEVELS - 2][2] = "Xen";
+		nodeSpecs[MAX_LEVELS - 2][3] = "0.15";
+		nodeSpecs[MAX_LEVELS - 2][4] = "3.0";
+		nodeSpecs[MAX_LEVELS - 2][5] = "0.05";
+		nodeSpecs[MAX_LEVELS - 2][6] = "0.1";
+		nodeSpecs[MAX_LEVELS - 2][7] = "true";
+		nodeSpecs[MAX_LEVELS - 2][8] = "false";
+		nodeSpecs[MAX_LEVELS - 2][9] = "2";
+		nodeSpecs[MAX_LEVELS - 2][10] = "5520";
+		nodeSpecs[MAX_LEVELS - 2][11] = "2000";
+		nodeSpecs[MAX_LEVELS - 2][12] = "512";
+		nodeSpecs[MAX_LEVELS - 3][0] = "x86";
+		nodeSpecs[MAX_LEVELS - 3][1] = "Linux";
+		nodeSpecs[MAX_LEVELS - 3][2] = "Xen";
+		nodeSpecs[MAX_LEVELS - 3][3] = "0.15";
+		nodeSpecs[MAX_LEVELS - 3][4] = "3.0";
+		nodeSpecs[MAX_LEVELS - 3][5] = "0.05";
+		nodeSpecs[MAX_LEVELS - 3][6] = "0.1";
+		nodeSpecs[MAX_LEVELS - 3][7] = "false";
+		nodeSpecs[MAX_LEVELS - 3][8] = "false";
+		nodeSpecs[MAX_LEVELS - 3][9] = "2";
+		nodeSpecs[MAX_LEVELS - 3][10] = "5520";
+		nodeSpecs[MAX_LEVELS - 3][11] = "1000";
+		nodeSpecs[MAX_LEVELS - 3][12] = "256";
+		nodeSpecs[MAX_LEVELS - 4][0] = "x86";
+		nodeSpecs[MAX_LEVELS - 4][1] = "Linux";
+		nodeSpecs[MAX_LEVELS - 4][2] = "Xen";
+		nodeSpecs[MAX_LEVELS - 4][3] = "0.15";
+		nodeSpecs[MAX_LEVELS - 4][4] = "3.0";
+		nodeSpecs[MAX_LEVELS - 4][5] = "0.05";
+		nodeSpecs[MAX_LEVELS - 4][6] = "0.1";
+		nodeSpecs[MAX_LEVELS - 4][7] = "false";
+		nodeSpecs[MAX_LEVELS - 4][8] = "false";
+		nodeSpecs[MAX_LEVELS - 4][9] = "2";
+		nodeSpecs[MAX_LEVELS - 4][10] = "5520";
+		nodeSpecs[MAX_LEVELS - 4][11] = "1000";
+		nodeSpecs[MAX_LEVELS - 4][12] = "128";
+		nodeSpecs[MAX_LEVELS - 5][0] = "x86";
+		nodeSpecs[MAX_LEVELS - 5][1] = "Linux";
+		nodeSpecs[MAX_LEVELS - 5][2] = "Xen";
+		nodeSpecs[MAX_LEVELS - 5][3] = "0.15";
+		nodeSpecs[MAX_LEVELS - 5][4] = "3.0";
+		nodeSpecs[MAX_LEVELS - 5][5] = "0.05";
+		nodeSpecs[MAX_LEVELS - 5][6] = "0.1";
+		nodeSpecs[MAX_LEVELS - 5][7] = "true";
+		nodeSpecs[MAX_LEVELS - 5][8] = "false";
+		nodeSpecs[MAX_LEVELS - 5][9] = "2";
+		nodeSpecs[MAX_LEVELS - 5][10] = "5520";
+		nodeSpecs[MAX_LEVELS - 5][11] = "256";
+		nodeSpecs[MAX_LEVELS - 5][12] = "64";
+		nodeSpecs[MAX_LEVELS - 6][0] = "x86";
+		nodeSpecs[MAX_LEVELS - 6][1] = "Linux";
+		nodeSpecs[MAX_LEVELS - 6][2] = "Xen";
+		nodeSpecs[MAX_LEVELS - 6][3] = "0.15";
+		nodeSpecs[MAX_LEVELS - 6][4] = "3.0";
+		nodeSpecs[MAX_LEVELS - 6][5] = "0.05";
+		nodeSpecs[MAX_LEVELS - 6][6] = "0.1";
+		nodeSpecs[MAX_LEVELS - 6][7] = "true";
+		nodeSpecs[MAX_LEVELS - 6][8] = "false";
+		nodeSpecs[MAX_LEVELS - 6][9] = "2";
+		nodeSpecs[MAX_LEVELS - 6][10] = "5520";
+		nodeSpecs[MAX_LEVELS - 6][11] = "128";
+		nodeSpecs[MAX_LEVELS - 6][12] = "64";
+		nodeSpecs[MAX_LEVELS - 7][0] = "Mobile Device";
+		nodeSpecs[MAX_LEVELS - 7][1] = "Mobile Device";
+		nodeSpecs[MAX_LEVELS - 7][2] = "Mobile Device";
+		nodeSpecs[MAX_LEVELS - 7][3] = "0";
+		nodeSpecs[MAX_LEVELS - 7][4] = "0";
+		nodeSpecs[MAX_LEVELS - 7][5] = "0";
+		nodeSpecs[MAX_LEVELS - 7][6] = "0";
+		nodeSpecs[MAX_LEVELS - 7][7] = "false";
+		nodeSpecs[MAX_LEVELS - 7][8] = "true";
+		nodeSpecs[MAX_LEVELS - 7][9] = "0";
+		nodeSpecs[MAX_LEVELS - 7][10] = "0";
+		nodeSpecs[MAX_LEVELS - 7][11] = "0";
+		nodeSpecs[MAX_LEVELS - 7][12] = "0";
 	}
 }
